3bfe7b7665a31a5e8c589539fe2caa3c4a0dcdd1
Lam Nguyen <ngoclamnn@gmail.com>
2020-12-01 18:24:53 +0700

    add new logic code

--- js/db.js
+++ js/db.js
@@ -1,64 +1,83 @@
-(function() {
+var dbInvoice = new PouchDB('invoicetool');
+var remoteCouch = 'http://admin:Aa123456@ec2-3-0-184-56.ap-southeast-1.compute.amazonaws.com:5984/invoicetool';
+function addInvoice(text) {
+  var todo = {
+    _id: "1",
+    title: text,
+    document_type: "Invoice",
+    products: [
+      {
+        productId: "2",
+        price: 100,
+        quantity: 100
+      }
+    ]
+  };
+  dbInvoice.put(todo, function callback(err, result) {
+    if (!err) {
+      console.log('Successfully posted a invoice!');
+    } else {
+      console.log(`Failed posted a invoice! ${err}`);
+    }
+  });
+}
+function map(doc) {
+  // join artist data to albums
+  if (doc.document_type === 'Invoice') {
+    emit(0, null);
+    for (var i in doc.products) {
+      emit(Number(i) + 1, {_id: doc.products[i].productId});
+    }
+
+  }
+}
+async function getInvoices() {
+  try {
+    var result = await dbInvoice.query(map, { include_docs: true });
+    console.log(result)
+  } catch (err) {
+    console.log(err);
+  }
+}
+function addProduct(text) {
+  var todo = {
+    _id: "2",
+    name: text,
+    price: 200,
+    quantity: 200,
+    document_type: "Product"
+  };
+  dbInvoice.put(todo, function callback(err, result) {
+    if (!err) {
+      console.log('Successfully posted a product!');
+    }
+    else {
+      console.log(`Failed posted a product! ${err}`);
+    }
+  });
+}
+(function () {
 
   'use strict';
   var syncDom = document.getElementById('sync-wrapper');
 
   // EDITING STARTS HERE (you dont need to edit anything above this line)
 
-  var dbInvoice = new PouchDB('invoicetool');
-  var remoteCouch = 'http://admin:Aa123456@ec2-3-0-184-56.ap-southeast-1.compute.amazonaws.com:5984/invoicetool';
-
-  var dbProduct = new PouchDB('products');
-  var remoteCouch = 'http://admin:Aa123456@ec2-3-0-184-56.ap-southeast-1.compute.amazonaws.com:5984/products';
-
-  var dbProperties = new PouchDB('properties');
-  var remoteCouch = 'http://admin:Aa123456@ec2-3-0-184-56.ap-southeast-1.compute.amazonaws.com:5984/properties';
-
-
-
   dbInvoice.changes({
     since: 'now',
     live: true
-  }).on('change', showInvoices);
-
-  // We have to create a new todo document and enter it in the database
-  function addInvoice(text) {
-    var todo = {
-      _id: new Date().toISOString(),
-      title: text,
-      completed: false
-    };
-    dbInvoice.put(todo, function callback(err, result) {
-      if (!err) {
-        console.log('Successfully posted a todo!');
-      }
-    });
-  }
-  // Show the current list of todos by reading them from the database
-  function showInvoices() {
-    dbInvoice.allDocs({include_docs: true, descending: true}, function(err, doc) {
-      redrawTodosUI(doc.rows);
-    });
-  }
+  }).on('change', getInvoices);
 
   // Initialise a sync with the remote server
   function sync() {
     syncDom.setAttribute('data-sync-state', 'syncing');
-    var opts = {live: true};
+    var opts = { live: true };
     dbInvoice.replicate.to(remoteCouch, opts, syncError);
     dbInvoice.replicate.from(remoteCouch, opts, syncError);
   }
-
-  // EDITING STARTS HERE (you dont need to edit anything below this line)
-
-  // There was some form or error syncing
   function syncError() {
     syncDom.setAttribute('data-sync-state', 'error');
   }
-
-
-
-
   if (remoteCouch) {
     sync();
   }
